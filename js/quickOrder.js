c6e7e705f61f9551f4835b670d486cf491720221
NewExample <timurmanilov12@mail.ru>
2021-06-22 08:07:16 +0300

    catalogPage

---
+++ js/quickOrder.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+$('.quickOrder').click(function () {
+        $('.blurBacground').removeClass('diActiveBackround');
+      });
+    $('.quickOrder').click(function () {
+        $('.formOrder').removeClass('diActiveBackround');
+      });  
+
+    $('.blurBacground').click(function () {
+        $('.blurBacground').addClass('diActiveBackround');
+        $('.formOrder').addClass('diActiveBackround');
+      });  
+    $('.blurBacground').click(function () {
+        $('.blurBacground').addClass('diActiveBackround');
+        $('.formOrder').addClass('diActiveBackround');
+      });    
+});      
